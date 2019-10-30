c7db756a029ad910010c1e1a2d8e6db8386ab06b
LucielVitorBS <luciel.vitor.bs@gmail.com>
2019-10-29 23:45:05 -0300

    Initial structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
+
+import { Main } from './models/Main'
+
+export default function App () {
+  return (
+    <>
+      <Main />
+    </>
+  );
+};
