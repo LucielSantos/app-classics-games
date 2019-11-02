4b0d01f3fa0ce0ee5b44c826726a8ef53594efd3
LucielVitorBS <luciel.vitor.bs@gmail.com>
2019-11-02 02:50:48 -0300

    button footer stop

--- src/models/stop/stop.js
+++ src/models/stop/stop.js
@@ -1,15 +1,63 @@
 import React from 'react';
 import {
   View,
-  Text
+  Text,
+  TouchableOpacity,
+  StyleSheet,
 } from 'react-native';
-
-import styles from './stop.styles'
 
 export const Stop = () => {
   return (
     <View style={styles.container}>
-      
+
+      <View style={styles.footer}>
+        <View  style={styles.containerButton}>
+          <TouchableOpacity style={styles.buttonStart}>
+            <Text style={styles.textButton}>Start</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor: '#262626',
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10
+  },
+  footer:{
+    width: 'auto',
+    backgroundColor: '#3348b5',
+    elevation: 10,
+    alignItems: 'center',
+    borderRadius: 100 ,
+    padding: 10,
+  },
+  containerButton:{
+    backgroundColor: '#0084d1',
+    elevation: 5,
+    borderRadius: 60,
+    height: 75,
+    width: 75,
+  },
+  buttonStart:{
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%'
+  },
+  textButton:{
+    color: 'white',
+    fontFamily: 'McLaren-Regular'
+  },
+  header:{
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+})
