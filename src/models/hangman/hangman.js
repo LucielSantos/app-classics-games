3dee2124d5eb59a789fd05a55e4d2ae2bed83918
LucielVitorBS <luciel.vitor.bs@gmail.com>
2019-11-20 00:27:07 -0300

    end, save on local storage

--- src/models/hangman/hangman.js
+++ src/models/hangman/hangman.js
@@ -9,6 +9,7 @@ import {
   Keyboard
 } from 'react-native';
 import Icon  from "react-native-vector-icons/MaterialIcons";
+import AsyncStorage from '@react-native-community/async-storage'
 
 import {hangmanImg} from '../../assets/images/hangmanImg'
 
@@ -21,12 +22,44 @@ export const Hangman = ({navigation }) => {
   const [dead, setDead] = useState(false)
   const [end, setEnd] = useState(false)
 
-  useEffect(() => {
-    setHangman(0)
-    
-  }, [step])
+  const [loading, setLoading] = useState(false)
 
-  const handleBeginGame = ({navigation}) => 
+  useEffect(() => {
+    const loadLocalData = async () =>{
+      try {
+        setLoading(true)
+
+        const localData = await AsyncStorage.getItem('dataHangman')
+        const data = JSON.parse(localData)
+
+        setStep(data.step)
+        setWord(data.word)
+        setLetter(data.letter)
+        setArrayWord(data.arrayWord)
+        setHangman(data.hangman)
+        setDead(data.dead)
+        setEnd(data.end)
+
+        setLoading(false)
+      } catch (error) {
+        console.log(error)
+      }
+
+    }
+
+    const load = async () => {
+      const verif = await AsyncStorage.getItem('dataHangman')
+
+      if(verif !== null){
+        await loadLocalData()
+      }
+    }
+
+    load()
+    
+  }, [])
+
+  const handleBeginGame = () => 
     step === 1 && (prepareWord(), setStep(2))
 
   const onChangeWord = text =>
@@ -60,16 +93,18 @@ export const Hangman = ({navigation }) => {
     setLetter('')
     const n = hangman+1 
     match ? setHangman(n) : ''
-    console.log(hangman)
     hangman === 5 ? setDead(true) : ''
     setEnd(isEnd)
   }
 
-  const handleBack = () =>
-    step === 1 ? navigation.goBack() : (setStep(1), setWord(''))
+  const handleBack = async () =>
+    step === 1 ? navigation.goBack() : (setStep(1), setWord(''), await AsyncStorage.removeItem('dataHangman'))
 
-  const handleSave = () =>
-    console.log('Salvando')
+  const handleSave = async () =>{
+    await AsyncStorage.setItem('dataHangman', JSON.stringify({
+      step, word, letter, arrayWord, hangman, dead, end,
+    }))
+  }
 
   const prepareWord = () => {
     const arrayWord = word.toUpperCase().split('')
@@ -80,82 +115,90 @@ export const Hangman = ({navigation }) => {
 
   return (
     <>
+    
     <View style={styles.container}>
-      <View style={styles.containerNavigate}>
-        <TouchableOpacity style={styles.button} onPress={handleBack}>
-          <Text style={styles.textButton}>{ step === 1 ? 'Sair' :  'Voltar' }</Text>
-        </TouchableOpacity>
-
-        {
-          step === 2 &&
-            <TouchableOpacity style={styles.button} onPress={handleSave} >
-              <Text style={styles.textButton}>Salvar</Text>
-            </TouchableOpacity>
-        }
-      </View>
-
-      <Text style={styles.title}>{ step === 1 ? 'Jogo da Forca' : 'O Jogo Começou! ;)'}</Text>
-      <View style={styles.body}>
-
-        {
-          step === 1 &&
+      {
+        loading
+        ? <Text style={styles.textBody}>Loading</Text>
+        :
           <>
-            <Text style={styles.textBody}>Insira uma Palavra para Começar o Jogo</Text>
-             
-            <TextInput
-              style={styles.input}
-              placeholder='Insira a palavra'
-              placeholderTextColor='#757575'
-              value= {word}
-              onChangeText={onChangeWord}
-            />
-
-            <TouchableOpacity style={styles.beginButton} onPress={handleBeginGame}>
-              <Text style={styles.textButton}>Começar o Jogo!</Text>
-            </TouchableOpacity>
-          </>
-        }
-
-        {
-          step === 2 &&
-          <>
-            <View style={styles.hangmanContainer}>
-              <Image
-                style={styles.img}
-                source={hangmanImg[hangman]}
-              />
-              <View style={styles.containerLetters}>
-                {
-                  arrayWord.map((obj, index) =>(
-                    obj.status ?
-                      <Text key={index} style={styles.letter}>{obj.letter}</Text>
-                    :
-                      <Text key={index} style={styles.letter}>_</Text>
-                  ))
-                }
-              </View>
-            </View>
-
-            <View style={styles.footer}>    
-              <TextInput
-                style={styles.inputLetter}
-                placeholder='Insira uma Letra'
-                placeholderTextColor='#757575'
-                value={letter}
-                onChangeText={onChangeLetter}
-                maxLength={1}
-              />
+            <View style={styles.containerNavigate}>
+              <TouchableOpacity style={styles.button} onPress={handleBack}>
+                <Text style={styles.textButton}>{step === 1 ? 'Sair' : 'Voltar'}</Text>
+              </TouchableOpacity>
 
               {
-                letter.length > 0 &&
-                <TouchableOpacity style={styles.containerEnter} onPress={onSubmit}>
-                  <Icon name='send' size={25} color='#3348b5' />
+                step === 2 &&
+                <TouchableOpacity style={styles.button} onPress={handleSave} >
+                  <Text style={styles.textButton}>Salvar</Text>
                 </TouchableOpacity>
               }
             </View>
+
+            <Text style={styles.title}>{step === 1 ? 'Jogo da Forca' : 'O Jogo Começou! ;)'}</Text>
+            <View style={styles.body}>
+
+              {
+                step === 1 &&
+                <>
+                  <Text style={styles.textBody}>Insira uma Palavra para Começar o Jogo</Text>
+
+                  <TextInput
+                    style={styles.input}
+                    placeholder='Insira a palavra'
+                    placeholderTextColor='#757575'
+                    value={word}
+                    onChangeText={onChangeWord}
+                  />
+
+                  <TouchableOpacity style={styles.beginButton} onPress={handleBeginGame}>
+                    <Text style={styles.textButton}>Começar o Jogo!</Text>
+                  </TouchableOpacity>
+                </>
+              }
+
+              {
+                step === 2 &&
+                <>
+                  <View style={styles.hangmanContainer}>
+                    <Image
+                      style={styles.img}
+                      source={hangmanImg[hangman]}
+                    />
+                    <View style={styles.containerLetters}>
+                      {
+                        arrayWord.map((obj, index) => (
+                          obj.status ?
+                            <Text key={index} style={styles.letter}>{obj.letter}</Text>
+                            :
+                            <Text key={index} style={styles.letter}>_</Text>
+                        ))
+                      }
+                    </View>
+                  </View>
+
+                  <View style={styles.footer}>
+                    <TextInput
+                      style={styles.inputLetter}
+                      placeholder='Insira uma Letra'
+                      placeholderTextColor='#757575'
+                      value={letter}
+                      onChangeText={onChangeLetter}
+                      maxLength={1}
+                    />
+
+                    {
+                      letter.length > 0 &&
+                      <TouchableOpacity style={styles.containerEnter} onPress={onSubmit}>
+                        <Icon name='send' size={25} color='#3348b5' />
+                      </TouchableOpacity>
+                    }
+                  </View>
+                </>
+              }
+            </View>
           </>
-        }
-    </View>
+      }
     </View>
     {
       dead &&
